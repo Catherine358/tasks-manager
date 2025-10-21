4859d3dd83637ea66c00a429c6de902c2160cd94
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2025-10-21 20:02:41 +0200

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TaskQueue from "./pages/TaskQueue.tsx";
+import TaskDetails from "./pages/TaskDetails.tsx";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<TaskQueue />} />
+              <Route path="/task/:id" element={<TaskDetails />} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
